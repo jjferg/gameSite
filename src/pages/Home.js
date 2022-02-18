d914919f74352d449cb2f992eb84a16baa298818
Your Name <mrjamaalferguson@yahoo.com>
2022-02-18 12:38:24 -0800

    changing layout for mobile

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,11 @@
-import React, { useEffect, useRef, useLayoutEffect } from "react";
+import React, { useEffect, useRef } from "react";
+import { Container, Row, Col } from "react-bootstrap";
 import NavigationBar from "../components/NavigationBar";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { CSSPlugin } from "gsap/CSSPlugin";
-import { ReactComponent as Logo } from "../Hullagahn_logo5.svg";
 import xboxController from "../components/images/xboxcontrol.png";
 import ps5Controller from "../components/images/ps5controller.png";
-
 
 gsap.registerPlugin(ScrollTrigger);
 gsap.registerPlugin(CSSPlugin);
@@ -21,79 +20,29 @@ const Home = () => {
 
   const tl = gsap.timeline();
 
-// animate spinning Logo
+  // animate spinning Logo
   useEffect(() => {
-      gsap.to(appLogoRef.current, {
-        ease: "none",
-        yoyo: true,
-        duration: 5,
-        repeat: -1,
-        rotationY: "360deg",
-      });
-  }, []);
-
-
-  useLayoutEffect(() => {
-    tl.to(xboxControlRef.current, {
-      scrollTrigger: {
-        trigger: xboxControlRef.current,
-        start: "top center",
-        onEnter: () => {
-          gsap.to(section1Ref.current, {
-            duration: 1.0,
-            backgroundColor: "#000000",
-          });
-        },
-        onEnterBack: () => {
-          gsap.to(section1Ref.current, {
-            duration: 1.0,
-            backgroundColor: "#ffffff",
-          });
-        },
-        scrub: true,
-      },
-      autoAlpha: 1,
-      x: 250,
+    gsap.to(appLogoRef.current, {
+      ease: "none",
+      yoyo: true,
+      duration: 5,
+      repeat: -1,
+      rotationY: "360deg",
     });
   }, []);
 
-  
+  useEffect(() => {}, []);
 
   return (
     <>
       <NavigationBar />
-      { <div className="container backGround">
-        {/* <Logo
-          ref={appLogoRef}
-          style={{
-            height: "5rem",
-            position: "absolute",
-            right: "15px",
-            top: "15px",
-          }}
-        /> */}
-      </div> }
-      <div className="section1" ref={section1Ref}>
-        {/* <img
-          className="xboxController"
-          src={xboxController}
-          alt="xbox elite controller"
-          ref={xboxControlRef}
-        /> */}
-        {/* <img
-          className="ps5Controller"
-          src={ps5Controller}
-          alt="playstation 5 white controller"
-        /> */}
-      </div>
-      <div
-        className="starVid"
-        ref={startElementRef}
-     
-       
-        preload="auto"
-      >
-       
+      <div className="mainDiv" >
+        <Row>
+          <Col sm={true} className="section1" >Yooo</Col>
+          <Col sm={true} className="section2">
+            its us in here!!
+          </Col>
+        </Row>
       </div>
     </>
   );
