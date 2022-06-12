d8d26c06b6027a8cae2160ff627cd577ec9f1076
Your Name <mrjamaalferguson@yahoo.com>
2022-06-12 16:31:34 -0700

    animation for home screen

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,11 +1,10 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import gsap from "gsap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { Card, Container } from "react-bootstrap";
+import { Card, Container, Row, Col } from "react-bootstrap";
 import gamePic from "../components/images/PNG/elden_ring_test1.png";
 import gameVid from "../components/images/PNG/eldenRingVideo.mp4";
-import { useLayoutEffect } from "react";
 import "../App.css";
 gsap.registerPlugin(ScrollTrigger);
 
@@ -14,79 +13,100 @@ const Home = () => {
   const welcomeTextEl = useRef();
   const gameVidEl = useRef();
   const card1El = useRef();
+  const weGame = useRef();
+  const circle = useRef();
 
-  useLayoutEffect(() => {
-    //gsap animations 
+  useEffect(() => {
+    //gsap animations
     const tl = gsap.timeline();
     tl.from(gameVidEl.current, {
       autoAlpha: 0,
       duration: 1,
       delay: 0.8,
-    }).from(welcomeTextEl.current, {
-      autoAlpha: 0,
-      duration: 1.5,
-      delay: 2,
-    });
-    gsap.from(card1El.current, {
-      autoAlpha: 0,
-      duration: 1.5,
-      y: "10%",
-      scrollTrigger: {
-        trigger: card1El.current,
-        start: "top 80%",
-      },
-    });
+    })
+      .from(welcomeTextEl.current, {
+        autoAlpha: 0,
+        duration: 1.5,
+        delay: .5,
+      })
+      gsap.from(card1El.current, {
+        autoAlpha: 0,
+        duration: 1.5,
+        y: "15%",
+        scrollTrigger: {
+          trigger: card1El.current,
+          start: "center 80%",
+        },
+      })
+      gsap.from(circle.current, {
+        autoAlpha: 0,
+        duration: 2.5,
+        x: "40%",
+        scrollTrigger: {
+          trigger: circle.current,
+          start: "center 80%",
+        },
+      })
+      gsap.from(weGame.current, { 
+        autoAlpha: 0, duration: 1.5, x: "-40%", scrollTrigger: {
+          trigger: circle.current,
+          start: "center 80%",
+        }
+      })
+      
   });
 
   return (
     <>
-      <div className="nav-crossbar"
-
-      ></div>
-      <Container className="container1" >
-        <div
-        className="vertical-bar"
-        ></div>
-
-        <div
-          ref={gameVidEl}
-          className="mt-4 video-el   bg-dark text-white rounded"
-
-        >
-          <video
-          className="video1"
-            autoPlay={true}
-            loop={true}
-            controls={false}
-            playsInline
-            muted
+      <div className="nav-crossbar"></div>
+      <div className="vertical-bar"></div>
+      <Container className="container1">
+        <Row>
+          <div
             ref={gameVidEl}
-            src={gameVid}
-            type="video/mp4"
-            
-          />
-          <h1
-            className="welcome-text"
-            ref={welcomeTextEl}
+            className="mt-4 video-el bg-dark text-white rounded"
           >
-            A HULLAGHAN'S WELCOME!
-          </h1>
-        </div>
-        <Card
-          ref={card1El}
-          className="mt-4"
-          style={{ width: "18rem", visibility: "hidden" }}
-        >
-          <Card.Img variant="top" src={gamePic} />
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-          </Card.Body>
-        </Card>
-        <div>Hulla Verse</div>
+            <video
+              className="video1"
+              autoPlay={true}
+              loop={true}
+              controls={false}
+              playsInline
+              muted
+              ref={gameVidEl}
+              src={gameVid}
+              type="video/mp4"
+            />
+            <h1 className="welcome-text" ref={welcomeTextEl}>
+              A HULLAGHAN'S WELCOME!
+            </h1>
+          </div>
+        </Row>
+        <Row className="align-items-center">
+          <Col>
+            <Card
+              ref={card1El}
+              className="mt-4"
+              style={{ width: "18rem", visibility: "hidden" }}
+            >
+              <Card.Img variant="top" src={gamePic} />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <div ref={circle} className="circle m-4.5 ">
+              <div ref={weGame} className="we-game ">
+                We gaming over Here Son
+              </div>
+            </div>
+          </Col>
+        </Row>
         <div>Hulla Verse</div>
         <div>Hulla Verse</div>
         <div>Hulla Verse</div>
