9a62f1bdfb5d1faf038b60877b9cb9e640cda1d0
Your Name <mrjamaalferguson@yahoo.com>
2022-08-08 11:26:01 -0700

    change color scheme and added the start of the final animation

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -246,7 +246,7 @@ const Home = () => {
       animation: tl3,
       trigger: divToPin.current,
       start: "top top",
-      end: "500",
+      end: "2500",
       pinSpacer: false,
       normalizeScroll: true,
       scrub: 0.004,
